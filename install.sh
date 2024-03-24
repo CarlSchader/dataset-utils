9d4474328d110a8e99291b9bb2d65ebaecf7a926
carlschader <carlschader@gmail.com>
2024-03-24 12:55:47 -0600

    updated the install script to own the target dir

--- install.sh
+++ install.sh
@@ -4,7 +4,14 @@ bins=(flatten ttv-dir merge-data-folders scrub-non-images)
 
 cargo build --release
 
+chown -R $SUDO_USER target
+
+install_dir=/usr/local/bin
+
 for bin in "${bins[@]}"
 do
-  cp target/release/$bin /usr/local/bin
+  cp target/release/$bin $install_dir
 done
+
+
+
